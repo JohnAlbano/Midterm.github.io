1922bc8f820e39f3096f26537db7ac818b50278a
JohnAlbano <71373784+JohnAlbano@users.noreply.github.com>
2020-11-19 10:35:55 +0800

    Update midterm.js

--- midterm.js
+++ midterm.js
@@ -1,17 +1,17 @@
 function getinfo(){
 	var inputa=document.getElementById('inputa').value;
 	var inputb=document.getElementById('inputb').value;
-	var inputa=document.getElementById('inputc').value;
+	var inputc=document.getElementById('inputc').value;
 	var output=document.getElementById("output");
 	var textarea=document.getElementById("input")
 	if (input <= 0 || input > 1000){
-		alert('Please enter valid number')
+		alert("Please enter valid number")
 	}
 	else {
 		output.innerHTML=" ";
 		output.style.display="block";
 		for(var inputa = 0; inputa <= inputb; inputa++){
-			var result = `${input} * $(inputc} = ${inputc*input}`
+			var result = `${inputa} * ${inputc} = ${inputc*inputa}`;
 			output.innerHTML+=result;
 		}
 		textarea.value=" ";
